91dcf224801526c5c326f7629e19afc9d91e991d
edapx <lastexxit@gmail.com>
2017-02-24 17:28:01 +0100

    put camera movement in a separate file

---
+++ src/scenography.js
@@ -0,0 +1,41 @@
+export default class Scenography {
+    constructor(camera, spline, t, cameraHeight, cameraSpeed){
+        this.spline = spline;
+        this.camera = camera;
+        this.t = t;
+        this.cameraHeight = cameraHeight;
+        this.cameraSpeed = cameraSpeed;
+    }
+
+    update(modus){
+        switch(modus){
+        case 1:
+            this.moveCamera();
+            break;
+        default:
+            return;
+            break;
+        }
+    }
+
+    moveCamera() {
+        var camPos = this.spline.getPoint(this.t);
+        this.camera.position.set(camPos.x, this.cameraHeight, camPos.z);
+
+        // the lookAt position is just 20 points ahead the current position
+        // but when we are close to the end of the path, the look at point
+        // is the first point in the curve
+        var next = this.t + this.cameraSpeed * 20;
+        var lookAtPoint = (next > 1) ? 0 : next;
+        var look = this.spline.getPoint(lookAtPoint);
+        look.y = this.cameraHeight;
+        this.camera.lookAt(look);
+
+        var limit = 1 - this.cameraSpeed;
+        this.t = (this.t >= limit) ? 0 : this.t += this.cameraSpeed;
+    }
+
+}
+
+
+
